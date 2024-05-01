eeb105bf00bbb21e5b312a6aa4cb7f636396b97c
Ana Luiza <aninhapmarques03@gmail.com>
2024-05-01 19:54:47 -0300

    salvamento de dados em arquivo txt

--- src/Model/BatalhaNaval.java
+++ src/Model/BatalhaNaval.java
@@ -1,15 +1,22 @@
 package Model;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class BatalhaNaval {
     private static final int TAMANHO_TABULEIRO = 15;
-    private static final int NUM_NAVIOS = 3;
+    private static final int NUM_NAVIOS = 5;
     private static final char[][] tabuleiro1 = new char[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
     private static final char[][] tabuleiro2 = new char[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
+    private static String[] navios1 = new String[NUM_NAVIOS];
+    private static String[] navios2 = new String[NUM_NAVIOS];
+    private static String[] tiros1 = new String[225];
+    private static String[] tiros2 = new String[225];
+    private static int qtd_tiros1;
+    private static int qtd_tiros2;
     private static int naviosRestantes1 = NUM_NAVIOS;
     private static int naviosRestantes2 = NUM_NAVIOS;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Jogador 1, posicione seus navios:");
         inicializarTabuleiro(tabuleiro1);
         exibirTabuleiro(tabuleiro1);
@@ -17,16 +24,22 @@ public class BatalhaNaval {
         System.out.println("Jogador 2, posicione seus navios:");
         inicializarTabuleiro(tabuleiro2);
         exibirTabuleiro(tabuleiro2);
+        
+        SalvarArquivo.EscreverArq(navios1,tiros1,qtd_tiros1,navios2,tiros2,qtd_tiros2);
 
         while (naviosRestantes1 > 0 && naviosRestantes2 > 0) {
             System.out.println("Vez do Jogador 1:");
             naviosRestantes2 = realizarJogada(tabuleiro2, naviosRestantes2);
+            SalvarArquivo.EscreverArq(navios1,tiros1,qtd_tiros1,navios2,tiros2,qtd_tiros2);
             System.out.println("\nNavios restantes do Jogador 2: " + naviosRestantes2);
             if(naviosRestantes2 == 0) break;
             System.out.println("Vez do Jogador 2:");
             naviosRestantes1 = realizarJogada(tabuleiro1, naviosRestantes1);
+            SalvarArquivo.EscreverArq(navios1,tiros1,qtd_tiros1,navios2,tiros2,qtd_tiros2);
+            SalvarArquivo.LerArq();
             System.out.println("\nNavios restantes do Jogador 1: " + naviosRestantes1);
         }
+        
 
         if (naviosRestantes1 == 0) {
             System.out.println("Parabéns, Jogador 2! Você afundou todos os navios do Jogador 1!");
@@ -42,6 +55,12 @@ public class BatalhaNaval {
             while (!posicaoValida) {
                 System.out.print("Posicione o navio " + (i + 1) + " (coordenada letra número), por exemplo, A3: ");
                 String coordenada = scanner.nextLine().toUpperCase(); // Converte para maiúsculas para tratamento consistente
+                if (tabuleiro == tabuleiro1) {
+                	navios1[i] = coordenada;
+                }
+                else {
+                	navios2[i] = coordenada;
+                }
 
                 if (coordenada.length() < 2 || coordenada.length() > 3) {
                     System.out.println("Coordenada inválida. Tente novamente.");
@@ -81,7 +100,7 @@ public class BatalhaNaval {
     }
 
     private static void exibirTabuleiro(char[][] tabuleiro) {
-        System.out.println("    0  1  2  3  4  5  6  7  8  9 10 11 12 13 14");
+        System.out.println("    1  2  3  4  5  6  7  8  9 10 11 12 13 14 15");
         for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
             System.out.format("%c  ", (i + 'A'));
             for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
@@ -106,11 +125,19 @@ public class BatalhaNaval {
     private static int realizarJogada(char[][] tabuleiro, int naviosRestantes) {
         Scanner scanner = new Scanner(System.in);
         boolean jogadaValida = false;
-        
+        int i = 0;
         while (!jogadaValida) {
             System.out.print("Digite a coordenada (letra número), por exemplo, A3: ");
             String coordenada = scanner.nextLine().toUpperCase(); // Converte para maiúsculas para tratamento consistente
-
+            if (tabuleiro == tabuleiro2) {
+            	tiros1[i] = coordenada;
+            	qtd_tiros1++;
+            }
+            else {
+            	tiros2[i] = coordenada;
+            	qtd_tiros2++;
+            }
+            
             if (coordenada.length() < 2 || coordenada.length() > 3) {
                 System.out.println("Coordenada inválida. Tente novamente.");
                 continue;
@@ -151,6 +178,7 @@ public class BatalhaNaval {
                 System.out.println("Você errou.");
                 tabuleiro[linha][coluna] = 'O'; // Marcador de tentativa falha
             }
+            i++;
         }
         
         exibirTabuleiro(tabuleiro);
---
+++ src/Model/SalvarArquivo.java
@@ -0,0 +1,81 @@
+package Model;
+
+import java.io.*;
+
+public class SalvarArquivo{
+	public static void EscreverArq(String[] navios1, String[] tiros1, int qtd_tiros1, String[] navios2, String[] tiros2, int qtd_tiros2) throws IOException {
+		PrintWriter arq = null;
+		
+		arq = new PrintWriter(new FileWriter("dados_partida.txt"));
+		
+		arq.write("Jogador 1\n");
+		if (navios1 != null) {
+			for (int i =0; i<5;i++) {
+				arq.write(navios1[i]);
+				arq.write("\n");
+			}
+		}
+		arq.write("\n");
+		if (qtd_tiros1 != 0) {
+			for (int i =0; i<qtd_tiros1;i++) {
+				arq.write(tiros1[i]);
+				arq.write("\n");
+			}
+		}
+		arq.write("\n");
+		arq.write("Jogador 2\n");
+		if (navios2 != null) {
+			for (int i =0; i<5;i++) {
+				arq.write(navios2[i]);
+				arq.write("\n");
+			}
+		}
+		arq.write("\n");
+		if (qtd_tiros2 != 0) {
+			for (int i =0; i<qtd_tiros2;i++) {
+				arq.write(tiros2[i]);
+				arq.write("\n");
+			}
+		}
+		
+		arq.close();
+	}
+	
+	public static void LerArq() throws IOException{
+		BufferedReader arq = new BufferedReader(new FileReader("dados_partida.txt"));
+		int num_navios = 5;
+	    String[] navios1 = new String[num_navios];
+	    String[] navios2 = new String[num_navios];
+	    String[] tiros1 = new String[225];
+	    String[] tiros2 = new String[225];
+		String s;
+	    arq.readLine(); // le "jogador 1"
+		for(int i = 0; i<num_navios; i++) {
+			s = arq.readLine();
+			navios1[i] = s;
+			System.out.println(navios1[i]);
+		}
+		int i =0;
+		s = arq.readLine(); // le "\n"
+		System.out.println(s);
+		while(arq.readLine() != "\n") { //nao ta funcionando
+			s = arq.readLine();
+			tiros1[i] = s;
+			i++;
+		}
+		arq.readLine(); // le "\n"
+		for(i = 0; i<num_navios; i++) {
+			s = arq.readLine();
+			navios2[i] = s;
+		}
+		i = 0;
+		arq.readLine(); // le "\n"
+		while(arq.ready()) {
+			s = arq.readLine();
+			tiros2[i] = s;
+			i++;
+		}
+
+		arq.close();
+	}
+}
