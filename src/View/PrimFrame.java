69b77683164414a752126b4cbd5b8316e6abe1ab
Ana Luiza Marques <113355947+anapmarques@users.noreply.github.com>
2024-05-17 16:20:37 -0300

    mudei cor e botei quadrados

--- src/View/PrimFrame.java
+++ src/View/PrimFrame.java
@@ -75,48 +75,57 @@ public class PrimFrame extends JFrame {
             }
             
             // Desenhar os navios
-            // solucoes burras, vou refazer isso depois quando conseguir pensar
-            // cores tambem vao ser mudadas
-            // - ana
             for (int i = 0; i < 14; i++) {
             	for (int j = 0; j < 19; j++){
-            		int x = 40 + (j * CELULA_SIZE); // +40 para nao ficar colado na esquerda
-                    int y = down_y + 20 + (i * CELULA_SIZE); // +20 para ficar alinhado com o tabuleiro
+            		int x = 30 + (j * CELULA_SIZE); // +40 para nao ficar colado na esquerda
+                    int y = down_y + 30 + (i * CELULA_SIZE); // +20 para ficar alinhado com o tabuleiro
 
                     if (i == 0 && (j == 1 || j == 5 || j == 9 || j == 13 || j == 17)) {
                     	Rectangle2D.Double cell = new Rectangle2D.Double(x, y, CELULA_SIZE, CELULA_SIZE);
-                        g2d.setColor(Color.GREEN);
+                        g2d.setColor(Color.green.darker());
                         g2d.fill(cell);
+                        g2d.setColor(Color.black);
+                        g2d.draw(cell);
                     }
                     
                     if (i == 1 && j % 2 == 0) {
                 		Rectangle2D.Double cell = new Rectangle2D.Double(x, y, CELULA_SIZE, CELULA_SIZE);
-                        g2d.setColor(Color.GREEN);
+                        g2d.setColor(Color.green.darker());
                         g2d.fill(cell);
+                        g2d.setColor(Color.black);
+                        g2d.draw(cell);
                 	}
                     
                     if (i == 4 && j % 2 == 0 && j < 7) {
                     	Rectangle2D.Double cell = new Rectangle2D.Double(x, y, CELULA_SIZE, CELULA_SIZE);
-                        g2d.setColor(Color.BLUE);
+                        g2d.setColor(Color.blue);
                         g2d.fill(cell);
+                        g2d.setColor(Color.black);
+                        g2d.draw(cell);
                     }
                     
                     if (i == 7 && (j == 0 || j == 1 || j == 3 || j == 4 || j == 6 || j == 7)) {
                     	Rectangle2D.Double cell = new Rectangle2D.Double(x, y, CELULA_SIZE, CELULA_SIZE);
-                        g2d.setColor(Color.YELLOW);
+                        g2d.setColor(Color.yellow);
                         g2d.fill(cell);
+                        g2d.setColor(Color.black);
+                        g2d.draw(cell);
                     }
                     
                     if (i == 10 && j != 4 && j < 9) {
                     	Rectangle2D.Double cell = new Rectangle2D.Double(x, y, CELULA_SIZE, CELULA_SIZE);
-                        g2d.setColor(Color.ORANGE);
+                        g2d.setColor(Color.orange);
                         g2d.fill(cell);
+                        g2d.setColor(Color.black);
+                        g2d.draw(cell);
                     }
                     
                     if (i == 13 && j < 5) {
                     	Rectangle2D.Double cell = new Rectangle2D.Double(x, y, CELULA_SIZE, CELULA_SIZE);
-                        g2d.setColor(Color.BLACK);
+                        g2d.setColor(Color.orange.darker().darker().darker());
                         g2d.fill(cell);
+                        g2d.setColor(Color.black);
+                        g2d.draw(cell);
                     }
             	}
             }
